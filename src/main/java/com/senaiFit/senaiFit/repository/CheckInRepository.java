de978a07d433f79bb33ea756e25fb990821db0d2
Yuri Silva <yuricosta149@gmail.com>
2022-05-06 23:42:42 -0300

    ajusta query

--- src/main/java/com/senaiFit/senaiFit/repository/CheckInRepository.java
+++ src/main/java/com/senaiFit/senaiFit/repository/CheckInRepository.java
@@ -15,7 +15,7 @@ public interface CheckInRepository extends JpaRepository<CheckIn, Long> {
     @Query(value = "SELECT count(*) FROM check_in WHERE parceiros_id = ?3 AND data_check_in BETWEEN ?1 AND ?2", nativeQuery = true)
     Integer buscaCheckInMesParceiros(LocalDate dataInicio, LocalDate dataFim, Long idParceiros);
 
-    @Query(value = "select COUNT(DISTINCT usu.id) from usuario usu\n" +
+    @Query(value = "SELECT COUNT(DISTINCT usu.id) FROM usuario usu\n" +
             "INNER JOIN check_in che ON che.usuario_id = usu.id\n" +
             "WHERE usu.superou_limite_semanal = 1 AND che.data_check_in BETWEEN ?1 AND ?2", nativeQuery = true)
     Integer buscaUsuariosQueSuperaramLimite(LocalDate dataInicio, LocalDate dataFim);
